29eaf7964c4a463e65090a548b6915125917cf08
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-08 13:41:08 +0100

    change

--- arnaldo/objects.js
+++ arnaldo/objects.js
@@ -1,4 +1,4 @@
-// Ex 1
+// Ex 1.1
 
 let person = {
     firstName: "Arnaldo",
@@ -40,4 +40,21 @@ document.getElementById("result").innerHTML = `
 <p>${car.brand} ${car.model}</p>
 <p> Favourite Animal: <h2>${animal.animalName}</h2>`;
 
-Ex 2
+// Ex 1.2
+class Person {
+    firstName;
+    lastName;
+    constructor(fName, lName) {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+    fullName() {
+        return this.firstName + ` ` + this.lastName;
+    }
+}
+let person1 = new Person("Arnaldo", "Bismarques");
+document.getElementById("solution").innerHTML += `
+<h1>${person1.firstName} ${person1.lastName}</h1>`
+let person2 = new Person("Lukasz", "Lang");
+document.getElementById("solution").innerHTML += `
+<p>The name of this guy is ${person2.firstName} ${person2.lastName}`;
